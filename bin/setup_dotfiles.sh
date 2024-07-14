83909743da3627250bd0bee3a2a8d5ce72dce4f5
Nathan Vale <nathanvale@users.noreply.github.com>
2024-07-15 06:09:45 +1000

    add iterm2 plug integration to fish config

--- bin/setup_dotfiles.sh
+++ bin/setup_dotfiles.sh
@@ -8,3 +8,6 @@
 
 # dss
 echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
+
+curl -L https://iterm2.com/shell_integration/fish \
+	-o ~/.iterm2_shell_integration.fish
