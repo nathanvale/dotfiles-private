f1dbb5fd60389c0ad4c991a2537d253c3b02429a
Nathan Vale <nathanvale@users.noreply.github.com>
2024-08-11 14:17:18 +1000

    remove sudo timer from macos prefs

--- genie/scripts/macos_preferences_manage.sh
+++ genie/scripts/macos_preferences_manage.sh
@@ -247,15 +247,15 @@ fi
 
 osascript -e 'tell application "System Preferences" to quit'
 
-# Ask for the administrator password upfront
-sudo -v
+# # Ask for the administrator password upfront
+# sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until this script has finished
-while true; do
-    sudo -n true
-    sleep 60
-    kill -0 "$$" || exit
-done 2>/dev/null &
+# # Keep-alive: update existing `sudo` time stamp until this script has finished
+# while true; do
+#     sudo -n true
+#     sleep 60
+#     kill -0 "$$" || exit
+# done 2>/dev/null &
 
 case $action in
 set)
