448d331ab9f4df6db9fc324fb9f17feeafcfa94e
Nathan Vale <nathanvale@users.noreply.github.com>
2024-08-12 10:35:35 +1000

    improve install download script

--- install.sh
+++ install.sh
@@ -11,18 +11,48 @@ tmp_dir="/tmp/genie-$(date +%Y%m%d%H%M%S)"
 # Cleanup function to delete the temporary directory
 cleanup() {
     rm -rf /tmp/genie-*
+    if ! rm -rf "$tmp_dir"; then
+        echo "Failed to remove the temporary directory $tmp_dir"
+    else
+        echo "Removed the temporary directory $tmp_dir"
+    fi
 }
 
-# Function to download scripts
 download_script() {
-    if [ -z "$1" ] || [ -z "$2" ]; then
-        echo "Usage: download_script <url> <folder>"
-        exit 1
-    fi
     local url=$1
     local folder=$2
     local tmp_file="$tmp_dir/$folder/$(basename "$url")"
-    status_code=$(curl -w "%{http_code}" -s -o "$tmp_file" "$url")
+
+    # Determine whether to use the Authorization header
+    if [ -n "$GITHUB_TOKEN" ]; then
+        auth_header="Authorization: token $GITHUB_TOKEN"
+    else
+        auth_header=""
+    fi
+
+    # Download the script
+    echo "Downloading $url..."
+    if [ -n "$auth_header" ]; then
+        status_code=$(curl -w "%{http_code}" -H "$auth_header" -s -o "$tmp_file" "$url")
+    else
+        status_code=$(curl -w "%{http_code}" -s -o "$tmp_file" "$url")
+    fi
+
+    # Check if the download was successful
+    if [ "$status_code" -ne 200 ]; then
+        message = "Failed to download $url (status code: $status_code)"
+        echo -e "\033[0;31m$message\033[0m"
+        # Check if the GITHUB_TOKEN environment variable is not set
+        if [ -z "$auth_header" ]; then
+            echo "Make sure to set the GITHUB_TOKEN environment variable if $url exists in a private repo."
+            echo "For public repos, you can ignore this message."
+            echo "export GITHUB_TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+        fi
+        cleanup
+        exit 1
+    fi
+
+    # Make the script executable
     chmod +x "$tmp_file"
 }
 
