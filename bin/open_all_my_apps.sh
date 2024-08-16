a283dbd3cfe5853ac595dbf58568a374e0f35627
Nathan Vale <nathanvale@users.noreply.github.com>
2024-08-16 11:36:39 +1000

    add twitter to aerospace bindings
    - fix bug in open_all_my_apps.sh

--- bin/open_all_my_apps.sh
+++ bin/open_all_my_apps.sh
@@ -11,7 +11,7 @@ open_all_my_apps() {
         apps_to_open=("iTerm" "Visual Studio Code" "Google Chrome" "Safari" "Slack" "Microsoft Outlook" "Notes" "Reminders" "Microsoft Teams")
     else
         # Define an array of applications to open on my personal Mac
-        apps_to_open=("iTerm" "Visual Studio Code" "Google Chrome" "Safari" "Notes" "Reminders" "Calendar","Messages")
+        apps_to_open=("iTerm" "Visual Studio Code" "Google Chrome" "Safari" "Notes" "Reminders" "Calendar" "Messages")
     fi
 
     # Loop through each application in the list and open it
