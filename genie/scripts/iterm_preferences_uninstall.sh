ae7cfcb8a027df350f4f23205e81b267f74710be
Nathan Vale <nathanvale@users.noreply.github.com>
2025-07-10 12:27:29 +1000

    Remove obsolete SSH configuration files and scripts
    - Deleted multiple SSH config expected files that are no longer needed.
    - Removed installation scripts related to iTerm2 preferences and macOS preferences.
    - Eliminated scripts for managing Nerd Fonts and symlinks, as well as their corresponding uninstall scripts.
    - Cleared out uninstallation scripts and their related test files.

--- genie/scripts/iterm_preferences_uninstall.sh
+++
@@ -1,9 +0,0 @@
-# Ensure the script is being run from the correct directory
-cd "$(dirname "$0")"
-
-# Source the colour_log.sh script
-source "./colour_log.sh"
-
-log $INFO "Attempting to set remove iTerm2 preferences..."
-
-./iterm_preferences_manage.sh --delete
